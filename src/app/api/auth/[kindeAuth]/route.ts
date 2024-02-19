fd8df3cae16503496b0147c7e8c87a6898d0cbe8
dbEsq <d_ber1@live.com>
2024-02-18 23:39:34 -0600

    update kindeAuth route per Kinde documentation

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,5 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+export async function GET(request, {params}) {
+    const endpoint = params.kindeAuth;
+    return await handleAuth(request, endpoint);
+}
