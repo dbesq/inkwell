47225fa7ce3e636090fcb52eca5110bf6761049e
dbEsq <d_ber1@live.com>
2024-02-18 23:49:11 -0600

    update kindeAuth route per Kinde documentation 2

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -11,7 +11,7 @@ const t = initTRPC.create()
 // Custom middleware to check authentication
 const middleware = t.middleware
 const isAuth = middleware(async (opts) => {
-    const { getUser } = getKindeServerSession()
+    const { getUser } = await getKindeServerSession()
     const user = await getUser()
 
     if(!user || !user.id) {
