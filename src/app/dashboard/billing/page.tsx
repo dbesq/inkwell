1370d8df8ede412277c75c12178d426d8fedb435
dbEsq <d_ber1@live.com>
2024-02-20 01:41:51 -0600

    /dashboard/billing/page 2

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,10 +1,10 @@
 import BillingForm from "@/components/BillingForm"
-import { getUserSubscriptionPlan } from "@/lib/stripe"
+import { getUserSubscriptionPlan } from "@/lib/stripe";
 
-const Page = async () => {
-    const subscriptionPlan = await getUserSubscriptionPlan()
+const BillingPage = async () => {
+  const subscriptionPlan = await getUserSubscriptionPlan();
 
-    return <BillingForm subscriptionPlan={subscriptionPlan} /> 
-}
+  return <BillingForm subscriptionPlan={subscriptionPlan} />;
+};
 
-export default Page
+export default BillingPage;
